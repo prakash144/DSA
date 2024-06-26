3b7d0014ffe9b736be1051e724d8f1088af5426a
Prakash <prakash@Prakashs-MacBook-Air.local>
2024-06-26 18:33:28 +0530

    removed leetcode

--- leetcode/1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        B = list(itertools.accumulate(arr, func = operator.xor)) + [0]
-        return [B[L-1]^B[R] for L,R in queries]
-                
-                
-        
